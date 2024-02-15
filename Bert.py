cf2b8ad64d14641634be4e7e617b4b6404e632ac
TwistedPair <35606899+rahatbhambri@users.noreply.github.com>
2024-02-15 16:14:23 +0530

    Create Bert.py

---
+++ Bert.py
@@ -0,0 +1,60 @@
+import torch, requests
+from transformers import BertForQuestionAnswering
+from transformers import BertTokenizer
+from bs4 import BeautifulSoup
+
+
+#Model
+model = BertForQuestionAnswering.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
+
+#Tokenizer
+tokenizer = BertTokenizer.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
+
+question = '''Who was bhagat singh's partner in crime'''
+
+
+response = requests.get("https://en.wikipedia.org/wiki/Bhagat_Singh")
+soup = BeautifulSoup(response.content, "html.parser")
+
+# Find all paragraphs on the webpage
+paragraphs = soup.find_all("p")
+
+para = ""
+# Extract and print the text content of each paragraph
+for paragraph in paragraphs:
+    para += paragraph.text
+
+paragraph = para
+
+
+# Encode question and paragraph
+encoding = tokenizer.encode_plus(text=question, text_pair=paragraph, max_length=512, truncation=True, return_tensors="pt")
+
+# Perform inference
+start_scores, end_scores = model(**encoding, return_dict=False)
+
+# Get the index of the start and end tokens with the highest score
+start_index = torch.argmax(start_scores)
+end_index = torch.argmax(end_scores)
+
+# Get the tokens corresponding to the answer
+tokens = tokenizer.convert_ids_to_tokens(encoding['input_ids'].squeeze())
+answer_tokens = tokens[start_index:end_index+1]
+
+# Convert answer tokens back to string
+answer = tokenizer.convert_tokens_to_string(answer_tokens)
+
+
+print(paragraph)
+corrected_answer = ''
+
+for word in answer.split():
+    
+    #If it's a subword token
+    if word[0:2] == '##':
+        corrected_answer += word[2:]
+    else:
+        corrected_answer += ' ' + word
+
+print(corrected_answer)
+
