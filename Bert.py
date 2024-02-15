64b5df669a6a5c4ae34cddbdb1498ab601597819
TwistedPair <35606899+rahatbhambri@users.noreply.github.com>
2024-02-15 12:19:01 +0000

    qa changes

--- Bert.py
+++ Bert.py
@@ -2,6 +2,12 @@ import torch, requests
 from transformers import BertForQuestionAnswering
 from transformers import BertTokenizer
 from bs4 import BeautifulSoup
+import warnings
+
+# Suppress all warnings
+warnings.filterwarnings("ignore", message = "Be aware, overflowing tokens are not returned for the setting you have chosen.*" \
+, category=RuntimeWarning)
+
 
 
 #Model
@@ -10,10 +16,8 @@ model = BertForQuestionAnswering.from_pretrained('bert-large-uncased-whole-word-
 #Tokenizer
 tokenizer = BertTokenizer.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
 
-question = '''Who was bhagat singh's partner in crime'''
-
-
-response = requests.get("https://en.wikipedia.org/wiki/Bhagat_Singh")
+topic = input("Enter topic you want to enquire \n")
+response = requests.get(f"https://en.wikipedia.org/wiki/{topic.lower()}")
 soup = BeautifulSoup(response.content, "html.parser")
 
 # Find all paragraphs on the webpage
@@ -27,34 +31,50 @@ for paragraph in paragraphs:
 paragraph = para
 
 
-# Encode question and paragraph
-encoding = tokenizer.encode_plus(text=question, text_pair=paragraph, max_length=512, truncation=True, return_tensors="pt")
+def getAnswer(question):
+    # Encode question and paragraph
+    encoding = tokenizer.encode_plus(text=question, text_pair=paragraph, max_length=512, truncation=True, return_tensors="pt")
 
-# Perform inference
-start_scores, end_scores = model(**encoding, return_dict=False)
+    # Perform inference
+    start_scores, end_scores = model(**encoding, return_dict=False)
 
-# Get the index of the start and end tokens with the highest score
-start_index = torch.argmax(start_scores)
-end_index = torch.argmax(end_scores)
+    # Get the index of the start and end tokens with the highest score
+    start_index = torch.argmax(start_scores)
+    end_index = torch.argmax(end_scores)
 
-# Get the tokens corresponding to the answer
-tokens = tokenizer.convert_ids_to_tokens(encoding['input_ids'].squeeze())
-answer_tokens = tokens[start_index:end_index+1]
+    # Get the tokens corresponding to the answer
+    tokens = tokenizer.convert_ids_to_tokens(encoding['input_ids'].squeeze())
+    answer_tokens = tokens[start_index:end_index+1]
 
-# Convert answer tokens back to string
-answer = tokenizer.convert_tokens_to_string(answer_tokens)
+    # Convert answer tokens back to string
+    answer = tokenizer.convert_tokens_to_string(answer_tokens)
+    corrected_answer = ''
+
+    for word in answer.split():
+        #If it's a subword token
+        if word[0:2] == '##':
+            corrected_answer += word[2:]
+        else:
+            corrected_answer += ' ' + word
+
+    return corrected_answer
 
 
-print(paragraph)
-corrected_answer = ''
 
-for word in answer.split():
-    
-    #If it's a subword token
-    if word[0:2] == '##':
-        corrected_answer += word[2:]
+while True:
+    question = input("Enter question or 1 to print context or 0 to end \n")
+    if question == "0":
+        break
+    elif question == "1":
+        print(paragraph)
     else:
-        corrected_answer += ' ' + word
+        answer = getAnswer(question)
 
-print(corrected_answer)
+        print("Question:", question)
+        if answer == "[SEP]":
+            print("Answer:", "I don't know that")
+        else:
+            print("Answer:", answer)
+
+
 
