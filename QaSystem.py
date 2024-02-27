6cc25364df321a9371da54368c7756a2a8a83c9e
rahatbhambri97 <rahat.bhambri@alpha-grep.com>
2024-02-27 13:01:12 +0530

    no need to set topic

--- QaSystem.py
+++ QaSystem.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 import warnings, re, random
 import spacy
 
-# Suppress all warnings
+
 warnings.filterwarnings("ignore", message = "Be aware, overflowing tokens are not returned for the setting you have chosen.*" \
 , category=RuntimeWarning)
 nlp = spacy.load('en_core_web_sm')
@@ -14,9 +14,7 @@ nlp = spacy.load('en_core_web_sm')
 
 class QASystem:
     def __init__(self):
-        #Model
         self.model = BertForQuestionAnswering.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
-        #Tokenizer
         self.tokenizer = BertTokenizer.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
         self.context = None
         self.topic = None
@@ -28,10 +26,8 @@ class QASystem:
         self.topic = topic
         response = requests.get(f"https://en.wikipedia.org/wiki/{topic.lower()}")
         soup = BeautifulSoup(response.content, "html.parser")
-        # Find all paragraphs on the webpage
         paragraphs = soup.find_all("p")
         para = ""
-        # Extract and print the text content of each paragraph
         for paragraph in paragraphs:
             para += paragraph.text
         imgs = soup.find_all('img')
@@ -52,47 +48,37 @@ class QASystem:
         if question == "1":
             return self.context, None        
         
-         # Create doc object
         doc = model(question)
         s = ""
-        # Iterate over tokens in the document
         for token in doc:
             print(token, token.pos_)
-            # Check if token is a proper noun
             if token.pos_ == 'PROPN':
                 s += (str(token) + " ") 
         self.SetContext(s.strip())
         
-        # Encode question and paragraph
+        #Answering question based on context        
         encoding = self.tokenizer.encode_plus(text=question, text_pair=self.context, max_length=512, truncation=True, return_tensors="pt")
-        # Perform inference
         start_scores, end_scores = self.model(**encoding, return_dict=False)
-        # Get the index of the start and end tokens with the highest score
         start_index = torch.argmax(start_scores)
         end_index = torch.argmax(end_scores)
-        # Get the tokens corresponding to the answer
         tokens = self.tokenizer.convert_ids_to_tokens(encoding['input_ids'].squeeze())
         answer_tokens = tokens[start_index:end_index+1]
-        # Convert answer tokens back to string
         answer = self.tokenizer.convert_tokens_to_string(answer_tokens)
         corrected_answer = ''
 
         for word in answer.split():
-            #If it's a subword token
             if word[0:2] == '##':
                 corrected_answer += word[2:]
             else:
                 corrected_answer += ' ' + word
 
         pattern = r"\[\s*\d+\s*\]"
-        # Replace the pattern with an empty string
         corrected_answer = re.sub(pattern, "", corrected_answer)
         corrected_answer = corrected_answer if "[SEP]" not in corrected_answer else "I don't know that"
         
         i = random.randint(0, len(self.image)-1) if len(self.image) > 0 else -1
         curr_im = self.image[i] if i >= 0 else None
 
-        #print("res = ", corrected_answer, curr_im)
         return corrected_answer, curr_im
 
 
