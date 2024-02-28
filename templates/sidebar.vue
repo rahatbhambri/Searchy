86962349cb6dc0bc5270ee1585294a4c24bb48cc
rahatbhambri97 <rahat.bhambri@alpha-grep.com>
2024-02-28 18:52:17 +0530

    ch

---
+++ templates/sidebar.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="sidebar">
+      <ul>
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/about">About</router-link></li>
+        </ul>
+    </div>
+  </template>
+  
+  <script>
+    export default {
+        name: 'Sidebar',
+    };
+  </script>
+  
+  <style scoped>
+  /* Customize your sidebar styles here */
+  .sidebar {
+    background-color: #f5f5f5;
+    width: 200px;
+    padding: 10px;
+    position: fixed;
+    top: 0;
+    left: 0;
+    height: 100vh;
+  }
+  </style>
