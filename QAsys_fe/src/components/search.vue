8026c04c1b7b94292f42d7d0477f0b3498a108f4
rahatbhambri97 <rahat.bhambri@alpha-grep.com>
2024-09-24 23:13:14 +0530

    fix: sessions bug

--- QAsys_fe/src/components/search.vue
+++ QAsys_fe/src/components/search.vue
@@ -9,10 +9,10 @@
     <div class="col sidebar bg-gradient-light border border-primary rounded">
       <ul class="sidebar-ul">
         <li><router-link style="color: black" to="/">+ New Chat </router-link></li>
-        <li v-for="sess in sessions" :key=sess.id @click="changeSession(sess.id)">
-          <router-link style="color: black" to="/hello"
-            >Session [[ sess.id ]]</router-link
-          >
+        <li v-for="sess in sessions" :key="sess.id" @click="changeSession(sess.id)">
+          <!-- <router-link style="color: black" to="/hello" -->
+          Session [[ sess.id ]]
+          <!-- </router-link -->
         </li>
       </ul>
     </div>
@@ -46,7 +46,7 @@
               v-for="chat in chats"
               :key="chat.question"
               class="d-flex"
-              style="border: 0.1in solid darkblue; background-color : #ADD8E6;"
+              style="border: 0.1in solid darkblue; background-color: #add8e6"
             >
               <img :src="chat.img" id="curr_img" class="img-fluid" />
               <p class="alert alert-primary">
